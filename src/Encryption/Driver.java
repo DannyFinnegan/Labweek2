04deda1f959c09b42e083d95ffb0022409a08d5d
T00225685 <daniel.finnegan@students.ittralee.ie>
2023-01-26 15:49:16 +0000

    sdsdf

--- src/Encryption/Driver.java
+++ src/Encryption/Driver.java
@@ -4,7 +4,7 @@ public class Driver
 {
     public static void main(String[] args)
     {
-        String Sentence = RowColumn.encryptRowColumn("This is an encrypted message",4);
+        String Sentence = RowColumn.encryptRowColumn("This is an encrypted message",3);
         System.out.println(Sentence);
     }
 
--- src/Encryption/RowColumn.java
+++ src/Encryption/RowColumn.java
@@ -4,7 +4,7 @@ public class RowColumn {
     public static String encryptRowColumn(String plaintext, int numColumns)
     {
         plaintext=plaintext.toUpperCase();
-        StringBuilder encryptedText = new StringBuilder("");
+        StringBuilder encryptedText = new StringBuilder();
         for (int i=0;i<=plaintext.length()-1;i++)
         {
             if (!Character.isWhitespace(plaintext.charAt(i)))
